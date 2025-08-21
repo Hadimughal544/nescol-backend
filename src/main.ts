13b8e84856a56e47e5a3aa13b1a4d737d17e3ca7
Muzamil Ali <muzamil_ali92@yahoo.com>
2025-08-21 17:51:26 +0500

    hello

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
     Credentials: true,
   });
   await app.listen(3001, '0.0.0.0');

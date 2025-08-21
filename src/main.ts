dd3cf20ad9d2d86641f31d137d796ba89b046249
Muzamil Ali <muzamil_ali92@yahoo.com>
2025-08-21 20:43:02 +0500

    hello

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: [
+      'https://nescolutilities.co.uk', // your frontend domain
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     Credentials: true,
